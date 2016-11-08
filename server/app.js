703ddf935446d078978c2a3b13a4785c819f35ab
deainya <deainya@gmail.com>
2016-11-09 00:01:29 +0400

    next step

--- server/app.js
+++ server/app.js
@@ -35,6 +35,21 @@ app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
+app.get('/setup', function(req, res) {
+  // create a sample user
+  var nick = new User({
+    name: 'Nick Cerminara', 
+    password: 'password',
+    admin: true
+  });
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
 // API ROUTES -------------------
 // we'll get to these in a second
 
