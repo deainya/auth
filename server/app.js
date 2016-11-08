0d7278a36e5abbc4ea1325a10c58aa1f908e82e7
deainya <deainya@gmail.com>
2016-11-08 23:59:16 +0400

    fix

--- server/app.js
+++ server/app.js
@@ -9,17 +9,9 @@ var mongoose    = require('mongoose');
 
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
+var User   = require('./User'); // get our mongoose model
 
-// get an instance of mongoose and mongoose.Schema
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-// set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model('User', new Schema({
-  name: String,
-  password: String,
-  admin: Boolean
-}));
 
 // =======================
 // configuration =========
