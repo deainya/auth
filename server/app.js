1fd0bec85d93be45155d11c7e40a85d6421ff21c
deainya <deainya@gmail.com>
2016-11-09 00:26:22 +0400

    authenticate route

--- server/app.js
+++ server/app.js
@@ -63,6 +63,36 @@ apiRoutes.get('/', function(req, res) {
   res.json({ message: 'Welcome to the coolest API on earth!' });
 });
 
+// route to authenticate a user (POST http://localhost:port/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+  });
+});
+
 // route to return all users (GET http://localhost:port/api/users)
 apiRoutes.get('/users', function(req, res) {
   User.find({}, function(err, users) {
