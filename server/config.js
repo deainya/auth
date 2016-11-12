81a072f2054ef0269c1ae68980477604873ee9d2
deainya <deainya@gmail.com>
2016-11-12 22:24:17 +0400

    try

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.port || 3001,
   'secret': 'secretforrfbgoinitiative',
-  'database': 'mongodb://user:pass@localhost:27017/partnergo-dev' // 'mongodb://user:pass@localhost:27017/rfbgo-dev'
+  'database': 'mongodb://user:pass@localhost:27017/partnergo-dev'
+  // 'mongodb://user:pass@localhost:27017/rfbgo-dev'
 };
